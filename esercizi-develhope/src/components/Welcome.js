b06871db40680c4f342121f0c0c580d8e4d13b94
gabrimarch3 <gabrimarche@protonmail.com>
2023-04-30 21:29:56 +0200

    added and completed es.7

--- esercizi-develhope/src/components/Welcome.js
+++ esercizi-develhope/src/components/Welcome.js
@@ -4,7 +4,7 @@ class Welcome extends React.Component {
   render() {
     return (
       <div>
-        <p>Welcome, {this.props.name}</p>
+        <p>Welcome, <strong>{this.props.name}</strong></p>
         <p>Your age is {this.props.age}</p>
       </div>
     );
