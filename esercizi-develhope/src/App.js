8c0c3dbe4694e24a6a8b2c8003e357f25fe4c7f5
gabrimarch3 <gabrimarche@protonmail.com>
2023-05-05 17:24:40 +0200

    added and completed ex.14

--- esercizi-develhope/src/App.js
+++ esercizi-develhope/src/App.js
@@ -1,7 +1,10 @@
 import Welcome from "./components/Welcome";
+import Counter from "./components/Counter";
 
 function App() {
-  return <Welcome name="Jhon" age={18}/>;
+  return (<><Welcome name="Jhon" age={18}/>
+  <Counter />
+  </>);
 }
 
 export default App;
