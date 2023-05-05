e8730cc71caccdc8bfd598e1dfd9ea07121d8a6d
gabrimarch3 <gabrimarche@protonmail.com>
2023-05-05 17:53:02 +0200

    added and completed es.17

--- esercizi-develhope/src/components/Counter.js
+++ esercizi-develhope/src/components/Counter.js
@@ -1,17 +1,46 @@
 import { useEffect, useState } from "react";
 import CounterDisplay from "./CounterDisplay";
+import React from "react";
 
-const Counter = ({start, add, ms}) => {
-  const [count, setCount] = useState(start);
+// const Counter = ({start, add, ms}) => {
+//   const [count, setCount] = useState(start);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-        setCount(count => count + add);
-    }, ms);
-    return () => clearInterval(interval)
-  }, [])
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//         setCount(count => count + add);
+//     }, ms);
+//     return () => clearInterval(interval)
+//   }, [])
 
-  return <CounterDisplay count={count} />
-};
+//   return <CounterDisplay count={count} />
+// };
+
+class Counter extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        count: props.start
+      };
+      
+      this.interval = null;
+    }
+  
+    componentDidMount() {
+      this.interval = setInterval(() => {
+        this.setState(state => ({
+          count: state.count + this.props.add
+        }));
+      }, this.props.ms);
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.interval);
+    }
+  
+    render() {
+      return <CounterDisplay count={this.state.count} />;
+    }
+  }
 
 export default Counter;
