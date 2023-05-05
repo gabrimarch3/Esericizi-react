931c4e95d6f1ed024251ef08a0809fa120702b1e
gabrimarch3 <gabrimarche@protonmail.com>
2023-05-05 17:30:09 +0200

    fixed counter

--- esercizi-develhope/src/components/Counter.js
+++ esercizi-develhope/src/components/Counter.js
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
 
-  setInterval(() => {
-    setCount(count + 1)
-  }, 1000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+        setCount(count => count + 1);
+    }, 1000);
+    return () => clearInterval(interval)
+  }, [])
 
   return <h1>{count}</h1>
 };
