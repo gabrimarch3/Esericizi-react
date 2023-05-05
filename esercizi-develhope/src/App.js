3569668043416460d89a529ad1cccec06430b96f
gabrimarch3 <gabrimarche@protonmail.com>
2023-05-05 17:04:35 +0200

    added and completed ex.10

--- esercizi-develhope/src/App.js
+++ esercizi-develhope/src/App.js
@@ -1,7 +1,7 @@
 import Welcome from "./components/Welcome";
 
 function App() {
-  return <Welcome name="Gabriel" age={23}/>;
+  return <Welcome name="Gabriel" age={18}/>;
 }
 
 export default App;
--- esercizi-develhope/src/components/Welcome.js
+++ esercizi-develhope/src/components/Welcome.js
@@ -1,12 +1,22 @@
 import React from "react";
-import Age from "./Age"
+import Age from "./Age";
 
 class Welcome extends React.Component {
   render() {
     return (
       <div>
-        <p>Welcome, <strong>{this.props.name}</strong></p>
-        {this.props.age >= 18 && <Age age={this.props.age}/>}
+        <p>
+          Welcome, <strong>{this.props.name}</strong>
+        </p>
+        {this.props.age ? (
+          this.props.age >= 18 ? (
+            <Age age={this.props.age} />
+          ) : (
+            <p>Mi dispiace, non sei ancora maggiorenne</p>
+          )
+        ) : (
+          <p>Per favore inserisci la tua età.</p>
+        )}
       </div>
     );
   }
