0d30e17714c4085d54c00eff9135dfa8b5d71c2c
gabrimarch3 <gabrimarche@protonmail.com>
2023-05-06 15:02:05 +0200

    added and completed ex.23

---
+++ esercizi-develhope/src/components/UncontrolledLogin.js
@@ -0,0 +1,24 @@
+const UncontrolledLogin = () => {
+  const handleUncontrolledLogin = (event) => {
+    event.preventDefault();
+
+    const userName = event.target.username.value;
+    const password = event.target.password.value;
+    const isChecked = event.target.remember.checked;
+
+    console.log({ userName, password, isChecked });
+  };
+
+  return (
+    <div className="uncontrolled-form">
+      <form onSubmit={handleUncontrolledLogin}>
+        <input type="text" name="username" />
+        <input type="password" name="password" />
+        <input type="checkbox" name="remember" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default UncontrolledLogin;
