d987f3a0e67d44720edb7d2da273ae83d7b9b151
gabrimarch3 <gabrimarche@protonmail.com>
2023-05-06 02:44:43 +0200

    added and completed ex.20

--- esercizi-develhope/src/App.js
+++ esercizi-develhope/src/App.js
@@ -1,13 +1,15 @@
-import Welcome from "./components/Welcome";
+// import Welcome from "./components/Welcome";
 import Counter from "./components/Counter";
 import ClickCounter from "./components/ClickCounter";
 import ClickTracker from "./components/ClickTracker";
+import InteractiveWelcome from "./components/InteractiveWelcome";
 import "./App.css"
 
 function App() {
   return (
     <>
-      <Welcome name="Jhon" age={18} />
+      {/* <Welcome name="Jhon" age={18} /> */}
+      <InteractiveWelcome />
       <div className="counters">
       <Counter start={10} add={2} ms={2000} />
       <ClickCounter />
