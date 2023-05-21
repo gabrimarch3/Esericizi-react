9672c8a9c4847a8c4a9f1372e96bafa70cd6618f
gabrimarch3 <gabrimarche@protonmail.com>
2023-05-22 00:21:49 +0200

    added and completed exercise

--- esercizi-develhope/src/App.js
+++ esercizi-develhope/src/App.js
@@ -1,36 +1,13 @@
-// import Welcome from "./components/Welcome";
-import Counter from "./components/Counter";
-import ClickCounter from "./components/ClickCounter";
-import ClickTracker from "./components/ClickTracker";
-import InteractiveWelcome from "./components/InteractiveWelcome";
-import Login from "./components/Login";
-import TodoList from "./components/TodoList";
-import "./App.css";
-import GithubUser from "./components/GitHubUser";
-import GithubUserList from "./components/GitHubUserList";
-import FilteredList from "./components/FilteredList";
+import Counter from './components/Counter';
+import { Route, Routes, Link } from 'react-router-dom';
 
 function App() {
-  return (
-    <>
-      {/* <Welcome name="Jhon" age={18} /> */}
-      <InteractiveWelcome />
-      <Login />
-      <div className="counters">
-        <Counter start={10} add={1.5} ms={1000} />
-        <ClickCounter />
-        <ClickTracker />
-      </div>
-      <TodoList />
-      <div className="github-user">
-        <h2>Dettagli Account:</h2>
-        <GithubUserList/>
-      </div>
-      <div>
-        <FilteredList />
-      </div>
-    </>
-  );
+  return <div>
+          <Link to="/counter">Counter</Link>
+        <Routes>
+          <Route path='/counter' element={<Counter/>} />   
+        </Routes>
+        </div>
 }
 
 export default App;
