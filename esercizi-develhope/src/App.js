cda0bd89ffd01c04185403705da10c6030ff12d0
gabrimarch3 <gabrimarche@protonmail.com>
2023-05-22 00:41:59 +0200

    added and completed ex.48

--- esercizi-develhope/src/App.js
+++ esercizi-develhope/src/App.js
@@ -1,18 +1,20 @@
 import { Link, Route, Routes } from "react-router-dom";
 import ShowGithubUser from "./components/ShowGithubUser";
 
-
 function App() {
-  return <div>
-          <Routes>
-            <Route path="user/:username" element={<ShowGithubUser/>}/>
-          </Routes>
-          <Link to='/user/PuglisiAlfio'>Go to User PuglisiAlfio</Link>
-          <br />
-          <Link to='/user/yangus98'>Go to User Giuseppe Bianconi</Link>
-          <br />
-          <Link to='/user/gabrimarch3'>Go to User GabriMarche</Link>
-        </div>
+  return (
+    <div>
+      <Routes>
+        <Route path="user/:username" element={<ShowGithubUser />} />
+      </Routes>
+      <Link to="/user/PuglisiAlfio">Go to User PuglisiAlfio</Link>
+      <br />
+      <Link to="/user/yangus98">Go to User Giuseppe Bianconi</Link>
+      <br />
+      <Link to="/user/gabrimarch3">Go to User GabriMarche</Link>
+      <Route path="*" element={<p>Not found</p>} />
+    </div>
+  );
 }
 
 export default App;
