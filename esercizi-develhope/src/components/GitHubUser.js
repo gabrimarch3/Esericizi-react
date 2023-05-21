50c19fa2fc9638ffe5e2e34c4016145d56afdefa
gabrimarch3 <gabrimarche@protonmail.com>
2023-05-21 15:13:31 +0200

    added and completed ex.40

--- esercizi-develhope/src/components/GitHubUser.js
+++ esercizi-develhope/src/components/GitHubUser.js
@@ -1,35 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import { useGithubUser } from "./hook/UseGithubUser";
+function GithubUser(props) {
+    const {data} = useGithubUser(props.username)
 
-const GithubUser = ({ username }) => {
-  const [userData, setUserData] = useState(null);
-
-  useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const response = await fetch(`https://api.github.com/users/${username}`);
-        const data = await response.json();
-        setUserData(data);
-      } catch (error) {
-        console.error('Error fetching GitHub user data:', error);
-      }
-    };
-
-    fetchUserData();
-  }, [username]);
-
-  if (!userData) {
-    return <div>Loading user data...</div>;
-  }
-
-  return (
-    <div>
-      <h2>{userData.name}</h2>
-      <p>Username: {userData.login}</p>
-      <p>Followers: {userData.followers}</p>
-      <p>Following: {userData.following}</p>
-      <p>Public Repos: {userData.public_repos}</p>
-    </div>
-  );
-};
+    return <>
+            <div>
+                {data && <a href={data.html_url}><h1>{data.login}</h1></a>}
+            </div>
+        </>
+}
 
 export default GithubUser;
