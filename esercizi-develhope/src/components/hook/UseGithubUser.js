93424d33163d626dea9dc0a64151fda8e0a87fd3
gabrimarch3 <gabrimarche@protonmail.com>
2023-05-22 02:55:06 +0200

    completed exercise

--- esercizi-develhope/src/components/hook/UseGithubUser.js
+++ esercizi-develhope/src/components/hook/UseGithubUser.js
@@ -1,10 +1,10 @@
 import useSWR from "swr"
 
-const fetcher = (url) => fetch(url).then((response) => response.json());
+const fetcher = url => fetch(url).then(response => response.json())
 
-export function useGithubUser(username) {
-  const { data } = useSWR(`https://api.github.com/users/${username}`, fetcher);
-  return {
-    data,
-  };
+export function useGithubUser (username){
+    const {data} = useSWR(username ? `https://api.github.com/users/${username}` : null, fetcher)
+    return {
+        data
+    }
 }
